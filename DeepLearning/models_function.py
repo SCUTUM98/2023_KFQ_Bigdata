90cc80797d3fe5bcac89d067fa5f6b7eec2997f0
BONGEUNGU <rnqhsrms98@gmail.com>
2023-07-17 11:48:42 +0900

    create models

--- DeepLearning/models_function.py
+++ DeepLearning/models_function.py
@@ -20,3 +20,17 @@ def get_relu_softmax2(learning_rate):
     of = optimizers.Adam(learning_rate=learning_rate)
     model.compile(optimizer=of, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     return model
+
+def large_model_creation(model_kind):
+    if(model_kind == 1):
+        model = keras.Sequential([layers.Dense(10, activation='softmax')])
+    elif(model_kind == 2):
+        model = keras.Sequential([
+            layers.Dense(96, activation='relu'),
+            layers.Dense(96, activation='relu'),
+            layers.Dense(10, activation='softmax')
+        ])
+    
+    model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    
+    return model
