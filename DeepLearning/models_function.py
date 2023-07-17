87b407a63d83ea1c155945fd18f95b24197df8a5
BONGEUNGU <rnqhsrms98@gmail.com>
2023-07-17 11:19:15 +0900

    create models

--- DeepLearning/models_function.py
+++ DeepLearning/models_function.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import keras
-from keras import Sequential, layers
+from keras import Sequential, layers, optimizers
 
 def get_relu_softmax():
     model = Sequential([
@@ -9,4 +9,14 @@ def get_relu_softmax():
     ])
     
     model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+def get_relu_softmax2(learning_rate):
+    model = Sequential([
+        layers.Dense(512, activation='relu'),
+        layers.Dense(10, activation='softmax')
+    ])
+    
+    of = optimizers.Adam(learning_rate=learning_rate)
+    model.compile(optimizer=of, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     return model
